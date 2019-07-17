b520fca74f192a1a703d7c86660450d609c8a6b9
Joshua <joshuagomersallgebba@gmail.com>
2019-07-17 14:31:03 +0100

    Inital Commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,52 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+class Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.login("wE dID a Conn3Ctionz Fam"))
+  }
+
+  def game1 = Action {
+    Ok(views.html.game1("T"))
+  }
+  def game2 = Action {
+    Ok(views.html.Game2("wE dID a dawdz Fam"))
+  }
+  def game3 = Action {
+    Ok(views.html.Game3("wE dID a dawdz Fam"))
+  }
+  def game4 = Action {
+    Ok(views.html.Game4("wE dID a dawdz Fam"))
+  }
+  def game5 = Action {
+    Ok(views.html.Game5("wE dID a dawdz Fam"))
+  }
+  def game6 = Action {
+    Ok(views.html.Game6("wE dID a dawdz Fam"))
+  }
+  def game7 = Action {
+    Ok(views.html.Game7("wE dID a dawdz Fam"))
+  }
+  def game8 = Action {
+    Ok(views.html.Game8("wE dID a dawdz Fam"))
+  }
+  def game9 = Action {
+    Ok(views.html.Game9("wE dID a dawdz Fam"))
+  }
+  def game10 = Action {
+    Ok(views.html.Game10("wE dID a dawdz Fam"))
+  }
+  def game11 = Action {
+    Ok(views.html.Game11("wE dID a dawdz Fam"))
+  }
+  def game12 = Action {
+    Ok(views.html.Game12("wE dID a dawdz Fam"))
+  }
+  def game13 = Action {
+    Ok(views.html.Game13("wE dID a dawdz Fam"))
+  }
+
+}
